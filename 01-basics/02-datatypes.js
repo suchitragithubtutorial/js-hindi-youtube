52daebe7cde2881b04ef3c45373ddf9f5c854912
suchitragithubtutorial <dashsuchitra6@gmail.com>
2024-11-26 10:32:28 +0530

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)// we are using nodejs not broswer console
+
+console.log( 3
+    +3);
+console.log("hitesh")
+//code readability should be high
+/*
+number => 2 to power 53
+bigint
+string =>""
+boolean =>true/false
+null => standalone value
+undefined =>
+symbol => unique
+//object
+*/
+console.log(typeof age);
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
