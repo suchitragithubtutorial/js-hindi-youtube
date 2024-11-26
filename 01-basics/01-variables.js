25f941ff8fa87a5abe0504c716aea1ac1d5d4dfa
suchitragithubtutorial <dashsuchitra6@gmail.com>
2024-11-26 10:20:37 +0530

    changes done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,8 @@
+let accountId = 132236
+ let accountEmail = "sychitraDash@gmail.com"
+  let accountPassword = "1435"
+  let accountCity = "jajpur"
+  let accountState ;//output will show undefined
+ console.table([accountId,accountEmail,accountCity,accountState]);
+
+ //prefer not to use var beacause of issues in block scope and functional scope
