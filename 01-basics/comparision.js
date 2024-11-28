942a590731969de5e48d3f021577a3e37b9f72b8
suchitragithubtutorial <dashsuchitra6@gmail.com>
2024-11-28 09:28:39 +0530

    comparision done

---
+++ 01-basics/comparision.js
@@ -0,0 +1,16 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(null > 0);
+console.log(null == 0);
+console.log(2 != 1);
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+console.log(undefined > 0)
+//usually avoid this kind of code bcz it makes everything 
+//so confusing celan code is the motto here
